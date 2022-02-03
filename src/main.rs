91d889a32d0d92c7d201a2d06721fb1d3e3f931d
Peter Arato <it.arato@gmail.com>
2022-02-02 22:30:38 -0500

    Circle - page 12

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,23 @@ mod defs;
 mod ray;
 mod vec3;
 
+fn hit_sphere(center: Vec3, r: FloatT, ray: &Ray) -> bool {
+    let oc = ray.origin() - center;
+    let a = Vec3::dot(ray.direction(), ray.direction());
+    let b = Vec3::dot(oc, ray.direction()) * 2.0;
+    let c = Vec3::dot(oc, oc) - (r * r);
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
+}
+
 fn color(ray: &Ray) -> Vec3 {
-    let unit_dir = ray.direction().unit();
-    let t = (unit_dir.y + 1.0) * 0.5;
-    Vec3::unif(1.0 - t) + (Vec3::new(0.5, 0.7, 1.0) * t)
+    if hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, ray) {
+        Vec3::new(1.0, 0.0, 0.0)
+    } else {
+        let unit_dir = ray.direction().unit();
+        let t = (unit_dir.y + 1.0) * 0.5;
+        Vec3::unif(1.0 - t) + (Vec3::new(0.5, 0.7, 1.0) * t)
+    }
 }
 
 fn main() {
